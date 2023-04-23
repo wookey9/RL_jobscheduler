fbcafdc8d821e73c51c625db82a437b216cd7134
3oogi <3oogi.koo@gmail.com>
2023-04-23 23:00:32 +0900

    qlearning

---
+++ agent.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+
+import numpy as np
+
+
+class Agent:
+
+    def __init__(self, Q, mode="test_mode"):
+        self.Q = Q
+        self.mode = mode
+        self.n_actions = 8
+        self.eps = 1.0
+        self.gamma = 0.85
+        self.alpha = 0.1
+        self.reward = defaultdict(lambda: np.zeros(self.n_actions))
+        self.N = defaultdict(lambda: np.zeros(self.n_actions))
+        self.history = []
+        self.rewardSum = 0
+
+    def select_action(self, state):
+        """
+        Params
+        ======
+        - state: the current state of the environment
+
+        Returns
+        =======
+        - action: an integer, compatible with the task's action space
+        """
+
+        if (self.mode == 'test_mode') | (np.random.rand() > self.eps):
+            action = np.argmax(self.Q[state])
+        else:
+            action = np.random.choice(self.n_actions)
+        return action
+
+    def step(self, state, action, reward, next_state, done):
+
+        """
+        Params
+        ======
+        - state: the previous state of the environment
+        - action: the agent's previous choice of action
+        - reward: last reward received
+        - next_state: the current state of the environment
+        - done: whether the episode is complete (True or False)
+        """
+
+        # epsilon decay
+        if done:
+            if self.eps > 0.01:
+                self.eps -= 0.00002
+            if self.mode == "mc_control":
+                for s, a, r in self.history:
+                    self.Q[s][a] += (self.reward[s][a] - self.Q[s][a]) / self.N[s][a]
+                self.reward.clear()
+                self.history.clear()
+                self.rewardSum = 0
+        else:
+            if self.mode == "mc_control":
+                # self.reward[state][action] = reward + (self.reward[state][action] * self.gamma)
+                self.rewardSum = reward + (self.rewardSum * self.gamma)
+                self.reward[state][action] += self.rewardSum
+                self.N[state][action] += 1
+                self.history.append((state, action, reward))
+
+            else:
+                self.Q[state][action] += self.alpha * (
+                            reward + self.gamma * np.max(self.Q[next_state]) - self.Q[state][action])
+
--- macSimulator.py
+++ macSimulator.py
@@ -1,19 +1,62 @@
+import random
+from collections import defaultdict
+
 import numpy as np
 import time, os
 import multiprocess as mp
+import agent
+
+actType = [(2,1),(2,2),(4,1),(4,2),(6,1),(6,2),(8,1),(8,2)]
 
 class MacSimulator:
     def __init__(self):
         self.num_cell = 4
         self.num_core = 4
+        self.num_action = 8
         self.cell_list = []
 
-    def step(self, action):
+        self.Q = defaultdict(lambda: np.zeros(self.num_action))
+
+    def getState(self):
+        state = np.zeros((self.num_cell, 10))
+        cellid = 0
+        for cell in self.cell_list:
+            state[cellid][0] = (int)(cell.totSch / 100)
+            state[cellid][1] = (int)(cell.totTbs / 1000)
+            state[cellid][2] = (int)(cell.cycle_proc[0] / 1000)
+            state[cellid][3] = (int)(cell.cycle_proc[1] / 1000)
+            state[cellid][4] = (int)(cell.cycle_proc[2] / 1000)
+
+            cellid += 1
+        return state
+    def qLearning(self, action):
+        self.agent = agent.Agent(self.Q, "q-learning")
+        num_episodes = 100000
+        for i_episode in range(1, num_episodes +1):
+            state = self.getState()
+            for i in range(500):
+                action = self.agent.select_action(state)
+                cellid = 0
+                for cell in self.cell_list:
+                    cell.setMaxSchPdu(actType[action][0])
+                    cell.setNumCore(actType[action][1])
+                    cellid += 1
+
+                self.cell_list[0].run(int(time.time_ns() / 1000))
+                self.cell_list[1].run(int(time.time_ns() / 1000))
+                self.cell_list[2].run(int(time.time_ns() / 1000))
+                self.cell_list[3].run(int(time.time_ns() / 1000))
+
+
+
+
+
         self.cell_list.clear()
         for cellid in range(self.num_cell):
-            self.cell_list.append(Cell(int(np.random.rand()*256), action[cellid]))
+            self.cell_list.append(Cell(int(np.random.rand()*256)))
+        self.agent.select_action()
         for cell in self.cell_list:
-            cell.run()
+            cell.run(int(time.time_ns() / 1000))
 
         return next_state,reward,done
 
@@ -29,7 +72,7 @@ class Cell:
         self.totTbs = 0
         self.totSch = 0
         self.cycle_proc = (0,0,0)
-        self.cycle_slot_limit = 150000
+        self.cycle_slot_limit = 200000
         self.run_cnt = 0
         self.skip_cnt = 0
         self.num_core = numcore
@@ -39,10 +82,18 @@ class Cell:
                 self.ue_list.append(Ue(uid,10,True))
             else:
                 self.ue_list.append(Ue(uid, 10, False))
+    def setNumCore(self, numcore):
+        self.num_core = numcore
 
-    def rbAllocation(self, tbs):
-        for i in range(tbs):
-            time.sleep(0.01)
+    def setMaxSchPdu(self, max_schpdu):
+        self.max_schpdu = max_schpdu
+
+    def rbAllocation(self, tbs, mcsProcess):
+        self.mcsCalculation(mcsProcess)
+        time.sleep(tbs * 0.0001)
+
+    def mcsCalculation(self, numProcess):
+        time.sleep(numProcess * 0.002)
 
     def schedule(self, ue, leftTbs):
         schTbs = 0
@@ -53,7 +104,7 @@ class Cell:
 
         procs = []
         for c in range(self.num_core):
-            proc = mp.Process(target = self.rbAllocation, args = (int(schTbs / 100 / self.num_core),))
+            proc = mp.Process(target = self.rbAllocation, args = (schTbs / self.num_core ,10 / self.num_core,))
             procs.append(proc)
             proc.start()
 
@@ -66,6 +117,7 @@ class Cell:
         return schTbs
 
     def run(self, cycle_start):
+        random.shuffle(self.ue_list)
         self.ue_list.sort(key=lambda ue: ue.bo, reverse=True)
         self.ue_list.sort(key=lambda ue: self.run_cnt - ue.updateTime, reverse=True)
 
@@ -87,20 +139,19 @@ class Cell:
 
         if(cycle_proc >= self.cycle_slot_limit):
             self.skip_cnt += 1
-            print("[{}] sch : {}, acctbs : {}, curSch : {}, curTbs : {}, proc : {}, skip : {}".format(self.run_cnt, self.totSch, self.totTbs, schcnt, schTbsSum, cycle_proc, self.skip_cnt))
         else:
-            cur_min, cur_max, cur_sum = self.cycle_proc
             self.totSch += schcnt
             self.totTbs += schTbsSum
 
-            if((cur_min > cycle_proc) | (cur_min == 0)):
-                cur_min = cycle_proc
-            if(cur_max < cycle_proc) :
-                cur_max = cycle_proc
-            cur_sum += cycle_proc
-            self.cycle_proc = (cur_min, cur_max, cur_sum)
+        cur_min, cur_max, cur_sum = self.cycle_proc
+        if((cur_min > cycle_proc) | (cur_min == 0)):
+            cur_min = cycle_proc
+        if(cur_max < cycle_proc) :
+            cur_max = cycle_proc
+        cur_sum += cycle_proc
+        self.cycle_proc = (cur_min, cur_max, cur_sum)
 
-            print("[{}] sch : {}, acctbs : {}, curSch : {}, curTbs : {}, proc : {}, min : {}, max : {}, avg : {}, skip : {}".format(self.run_cnt, self.totSch, self.totTbs, schcnt, schTbsSum, cycle_proc, cur_min, cur_max, int(cur_sum / self.run_cnt), self.skip_cnt))
+        print("[{}] sch : {}, acctbs : {}, curSch : {}, curTbs : {}, proc : {}, min : {}, max : {}, avg : {}, skip : {}".format(self.run_cnt, self.totSch, self.totTbs, schcnt, schTbsSum, cycle_proc, cur_min, cur_max, int(cur_sum / self.run_cnt), self.skip_cnt))
 
 class Ue:
     def __init__(self, uid, bo, heavy) :
@@ -119,9 +170,9 @@ class Ue:
 
 
 print("Number of processors : ", mp.cpu_count())
-cell = Cell(256,4,1)
+cell = Cell(256,12,4)
 
-for i in range(1000):
+for i in range(500):
     cell.run(int(time.time_ns() / 1000))
 
 for ue in cell.ue_list:
